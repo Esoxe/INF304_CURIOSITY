1ff85442c2093cd44c0305b61af6d77265242e54
esoxe <minoudoudou26@gmail.com>
2024-11-25 19:00:18 +0100

    interprete

--- curiosity-test.c
+++ curiosity-test.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <curiosity.c>
+#include "curiosity.c"
 
 
 
@@ -18,7 +18,7 @@ int main(int argc,char **argv){
     FILE *test=fopen(argv[1],"r");
     FILE *terrain;
     FILE *Programme;
-    int nb_pas;
+    int nb_pas,i=0;
     char e;
     int x,y;
     char o;
@@ -46,5 +46,70 @@ int main(int argc,char **argv){
     errp=lire_programme(&prog,Programme);
     gestion_erreur_programme(errp);
     init_etat(&etat);
-
+      init_etat(&etat);
+    do {
+    res = exec_pas(&prog, &envt, &etat);
+    /* Affichage du terrain et du robot */
+    i++;
+    afficher_envt(&envt);
+    } while (res == OK_ROBOT && i<nb_pas);
+    switch (res) {
+        case OK_ROBOT:
+            if(i+1==nb_pas){
+                if(e=='N')
+                {
+                    if(envt.r.x==x && envt.r.y==y){
+                        if(envt.r.o==o){
+                        printf("OK fin du nombre de pas et rebot sur la position attendu avec la bonne orientation");
+                        }
+                        else printf("nombre de pas finit robot a la bonne case mais pas la bonne orientation orientation voulue: %c  orientation reel %c",o,envt.r.o);
+                    }
+                    else printf("nomnbre de pas finit mais le robot n'est pas sur la bonne case: case voulue : %d %d  case du robot: %d %d",x,y,envt.r.x,envt.r.y);
+                }
+        printf("nombre de pas finit le robot est sur le terrain mais le programme devait faire l'event %c",e);
+        }
+        else 
+            printf("Robot sur une case sur le terrain mais le robot n'a pas fait le nb de pas demander");
+        break;
+    case SORTIE_ROBOT:
+        if(e=='S'){
+            printf("OK");
+        }
+        else printf("Robot sortie mais le robot aurait du faire l'event %c",e);
+        break;
+    case ARRET_ROBOT:
+        if(e=='F'){
+            if(envt.r.x==x && envt.r.y==y){
+                if(envt.r.o==o){
+                    printf("OK");
+                }
+                else printf("Programme finit robot a la bonne case mais pas la bonne orientation orientation voulue: %c  orientation reel %c",o,envt.r.o);
+            }
+            else printf("Programme finit mais le robot n'est pas sur la bonne case: case voulue : %d %d  case du robot: %d %d",x,y,envt.r.x,envt.r.y);
+        }
+        printf("Programme finit le robot est sur le terrain mais le programme devait faire l'event %c",e);
+        break;
+    case PLOUF_ROBOT:
+        if(e=='P'){
+            printf("OK");
+        }
+        else printf("Robot tomber a l'eau mais le robot aurait du faire l'event %c",e);
+        break;
+    case CRASH_ROBOT:
+        if(e=='O'){
+            printf("OK");
+        }
+        else printf("Le robot s'est pris un rocher alors qu'il devait faire l'event %c",e);
+        break;
+    
+    case ERREUR_PILE_VIDE:
+        printf("ERREUR : pile vide\n");
+        break;
+    case ERREUR_ADRESSAGE:
+        printf("ERREUR : erreur d'adressage\n");
+        break;
+    case ERREUR_DIVISION_PAR_ZERO:
+        printf("ERREUR : division par 0\n");
+        break;
+  }
 }
